6b2eb7b89a1aafc2e6abe312b1551347a7dd61d2
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-24 19:14:13 +1300

    Implement routes

--- SwedishClubDashboard/src/App.tsx
+++ SwedishClubDashboard/src/App.tsx
@@ -1,14 +1,24 @@
 
 
-
+import { Routes, Route } from "react-router-dom"
 import './index.css'
+import Dashboard from './pages/Dashboard'
+import Tasks from './pages/Tasks'
+import Navbar from "./components/Navbar"
 
 function App() {
   
 
   return (
     <>
-      <div>Hello</div>
+
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Routes>
+      </div>
     </> 
   )
 }
