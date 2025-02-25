ca3854c09a411880ad97ad2cbe0e23af0af9b91e
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-25 19:44:51 +1300

    Import lucide and set new task fields

--- SwedishClubDashboard/src/components/NewTask.tsx
+++ SwedishClubDashboard/src/components/NewTask.tsx
@@ -1,3 +1,7 @@
+import { X } from "lucide-react";
+import { useState } from "react";
+
+
 
 interface NewTaskProps {
     isOpen: boolean
@@ -7,15 +11,17 @@ interface NewTaskProps {
 const NewTask: React.FC<NewTaskProps> = ({isOpen, onClose }) => {
     if (!isOpen) return null
 
-
+    const [text, setText] = useState("")
+    const [tags, setTags] = useState<string[]>([])
+    const [due, setDue] = useState("")
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-md z-50">
-          {/* Modal Container */}
+          
           <div className="bg-white p-6 rounded-2xl shadow-xl w-full max-w-lg relative">
             {/* Close Button */}
-            <button onClick={onClose} className="absolute top-4 right-4 text-gray-500 hover:text-gray-800">
-               
+            <button onClick={onClose} className="absolute top-4 right-4 text-gray-500 hover:text-gray-800 cursor-pointer">
+                <X />
             </button>
     
             {/* Modal Content */}
@@ -25,7 +31,7 @@ const NewTask: React.FC<NewTaskProps> = ({isOpen, onClose }) => {
               <textarea placeholder="Task Description" className="border rounded-lg p-2" rows={3}></textarea>
               <input type="date" className="border rounded-lg p-2" />
               <button type="submit" className="bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700">
-                Add Task
+                Create 
               </button>
             </form>
           </div>
